2038f321e544b8521d1ff2aa225e13a20093c6f5
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-11-29 10:25:51 -0300

    consumindo a API 	modified:   html/index.html 	modified:   html/index.js 	modified:   package.json 	modified:   server.js

--- html/index.js
+++ html/index.js
@@ -1,6 +1,9 @@
 async function getContent() {
     try {
         const response = await fetch('http://localhost:8000/ ')
+        //console.log(response)
+        const data = await response.json()
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.get('/', (req, res)=>{
     return res.json([
-        {name: "Bhryan"},
-        {name: 'Julio'}
+        {
+            name: "Bhryan"
+        },
+        {
+            name: 'Julio'
+        }
     ])
 })
 
